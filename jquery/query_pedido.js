633584f2a716d4afdacf57354c1c972d0d2f4ad1
JDuranG <javier.duran@getronics.com>
2023-11-29 16:13:44 +0800

    Actividad final

--- jquery/query_pedido.js
+++ jquery/query_pedido.js
@@ -1,25 +1,44 @@
-$(function(){
-    $("#pedidoForm").on("submit", function(e){
+$(function () {
+    $("#pedidoForm").on("submit", function (e) {
         let fname = $("#fname").val();
         let lname = $("#lname").val();
         if (fname == "" || lname == "") {
             if (fname == "") {
-                $("#labelName").css("color","red");
+                $("#labelName").css("color", "red");
             }
             if (lname == "") {
-                $("#labelLast").css("color","red");
+                $("#labelLast").css("color", "red");
             }
-            if(fname != ""){
-                $("#labelName").css("color","black");
+            if (fname != "") {
+                $("#labelName").css("color", "black");
             }
             if (lname != "") {
-                $("#labelLast").css("color","black");
+                $("#labelLast").css("color", "black");
             }
             e.preventDefault();
         } else {
-            $("#labelName").css("color","black");
-            $("#labelLast").css("color","black");
+            $("#labelName").css("color", "black");
+            $("#labelLast").css("color", "black");
             e.submit();
         }
     })
+
+    $("#size").on('change', function () {
+        console.log(this.value);
+        // Aqui va la llamada AJAX con JQuery
+        $.ajax({
+            url: 'http://localhost:5000/checksize',
+            dataType: "html",
+            type: 'POST',
+            data: "size=" + this.value,
+            success: function (data) {
+                $("#resultado_tamano").text(data);
+            }
+        });
+        // Llamada v2 (tambien funciona)
+        // valor = { size: this.value }
+        // $.post('http://localhost:5000/checksize', valor).then(resp => {
+        //     $("#resultado_tamano").text(resp);
+        // })
+    });
 });
